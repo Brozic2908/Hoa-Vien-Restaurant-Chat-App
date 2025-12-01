94b253c6e41cc828a50e7a9d4e6fd64e1325e113
Brozic2908 <nlqk2908@gmail.com>
2025-12-02 00:07:20 +0700

    Done

--- python/src/config.py
+++ python/src/config.py
@@ -12,25 +12,25 @@ class Config:
     COLLECTION_NAME = "hoa_vien_data"
     
     # Paths (Tương thích với Docker volume mount trong util.sh)
-    # INPUT_DIR = "/nlp/input"
-    # OUTPUT_DIR = "/nlp/output"
-    # DATA_DIR = "/nlp/data"
+    INPUT_DIR = "/nlp/input"
+    OUTPUT_DIR = "/nlp/output"
+    DATA_DIR = "/nlp/data"
     
     # Uncomment nếu muốn test trên vscode
     # 1. Lấy đường dẫn tuyệt đối của file config.py hiện tại
     # Ví dụ: /app/src/config.py
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-    # 2. "Lùi" một cấp để ra thư mục gốc của dự án (Project Root)
-    # Từ /app/src/ -> lùi về /app/
-    PYTHON_DIR = os.path.dirname(BASE_DIR)
-    ROOT_DIR = os.path.dirname(PYTHON_DIR)
+    # # 2. "Lùi" một cấp để ra thư mục gốc của dự án (Project Root)
+    # # Từ /app/src/ -> lùi về /app/
+    # PYTHON_DIR = os.path.dirname(BASE_DIR)
+    # ROOT_DIR = os.path.dirname(PYTHON_DIR)
 
-    # 3. Định nghĩa đường dẫn tới các thư mục Input/Output dựa trên ROOT_DIR
-    INPUT_DIR = os.path.join(ROOT_DIR, "input") 
+    # # 3. Định nghĩa đường dẫn tới các thư mục Input/Output dựa trên ROOT_DIR
+    # INPUT_DIR = os.path.join(ROOT_DIR, "input") 
     
-    # [cite_start]Yêu cầu đề bài: output/answer.txt [cite: 670]
-    OUTPUT_DIR = os.path.join(ROOT_DIR, "2211522/output")
+    # # [cite_start]Yêu cầu đề bài: output/answer.txt [cite: 670]
+    # OUTPUT_DIR = os.path.join(ROOT_DIR, "2211522/output")
     
-    # Đường dẫn tới thư mục data (chứa menu.json, v.v.)
-    DATA_DIR = os.path.join(PYTHON_DIR, "data")
+    # # Đường dẫn tới thư mục data (chứa menu.json, v.v.)
+    # DATA_DIR = os.path.join(PYTHON_DIR, "data")
